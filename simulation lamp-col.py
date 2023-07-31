7127cc8affb8719f915966998574c335f89b6fe9
jampsyn <141011885+jampsyn@users.noreply.github.com>
2023-07-31 11:29:43 +0300

    sim lamp-col.py

--- simulation lamp-col.py
+++ simulation lamp-col.py
@@ -2,14 +2,21 @@ import random
 import time
 
 def simulate(num_lamps, num_colors):
-    lamps = [random.randint(1, num_colors) for _ in range(num_lamps)]
+    # Randomly select a single color for all lamps
+    lamp_color = random.randint(1, num_colors)
     num_presses = 1
-    while len(set(lamps)) > 1:
-        lamps = [random.randint(1, num_colors) for _ in range(num_lamps)]
+    while True:
+        new_color = random.randint(1, num_colors)
         num_presses += 1
+        if new_color == lamp_color:
+            break
     return num_presses
 
 def run_simulations(num_simulations, num_lamps, num_colors):
+    if num_lamps <= 0 or num_colors <= 0:
+        print("The number of lamps and colors must be positive integers.")
+        return
+
     total_presses = 0
     max_presses = 0
     min_presses = float('inf')
@@ -18,7 +25,6 @@ def run_simulations(num_simulations, num_lamps, num_colors):
     start_time = time.time()
 
     simulation_times = {}
-
 
     for i in range(num_simulations):
         start_simulation_time = time.time()
@@ -32,29 +38,29 @@ def run_simulations(num_simulations, num_lamps, num_colors):
             min_simulation = i + 1
         end_simulation_time = time.time()
         simulation_times[i + 1] = end_simulation_time - start_simulation_time
-        print(f"Simulation {i+1}: = {presses}")
+        print(f"Simulation {i+1}: {presses}")
 
     average_presses = total_presses / num_simulations
     end_time = time.time()
     elapsed_time = end_time - start_time
 
-    print(f"\nortalama: {average_presses:.2f}")
-    print(f"max: {max_presses} (Simulation {max_simulation})")
+    print(f"\nAverage: {average_presses:.2f}")
+    print(f"Max: {max_presses} (Simulation {max_simulation})")
     print(f"Min: {min_presses} (Simulation {min_simulation})")
-    print(f"süre: {elapsed_time:.2f} seconds\n")
+    print(f"Total time: {elapsed_time:.2f} seconds\n")
 
     while True:
-        simulation_number = int(input("simulation sorgula (0=çık): "))
+        simulation_number = int(input("Query a simulation (0=exit): "))
         if simulation_number == 0:
             break
         elif simulation_number in simulation_times:
             simulation_time = simulation_times[simulation_number]
-            print(f"simulation {simulation_number} şu kadar sürmüş: {simulation_time:.2f} saniye")
+            print(f"Simulation {simulation_number} took {simulation_time:.2f} seconds")
         else:
-            print("doğru gir lan.")
+            print("Invalid simulation number.")
 
-num_simulations = int(input("simulation sayısı: "))
-num_lamps = int(input("lamba sayısı: "))
-num_colors = int(input("kaç renk olsun reis: "))
+num_simulations = int(input("Number of simulations: "))
+num_lamps = int(input("Number of lamps: "))
+num_colors = int(input("Number of colors: "))
 
 run_simulations(num_simulations, num_lamps, num_colors)
